feec925b137075f74b901cb6333263a2bcdb0bdc
professorholowczak@gmail.com <professorholowczak@gmail.com>
2025-12-04 16:36:38 +0000

    Refactored Agency Dimension to use CTE and ORDER BY

--- CIS_4400_Fall_2025_Project/models/agency_dimension.sql
+++ CIS_4400_Fall_2025_Project/models/agency_dimension.sql
@@ -1,10 +1,13 @@
 -- agency_dimension.sql 
+WITH agency_column AS
+( 
+     SELECT DISTINCT agency, agency_name
+     FROM {{ ref('raw_311_complaints') }}
+     ORDER BY 1, 2
+)
 SELECT 
     row_number() OVER () AS agency_dim_id,
     agency, agency_name, current_timestamp() as loaded_at
-FROM  
-    ( 
-         SELECT DISTINCT agency, agency_name
-         FROM {{ ref('raw_311_complaints') }}
-         ORDER BY 1, 2
-    )
+FROM  agency_columns
+ORDER BY agency, agency_name;
+
