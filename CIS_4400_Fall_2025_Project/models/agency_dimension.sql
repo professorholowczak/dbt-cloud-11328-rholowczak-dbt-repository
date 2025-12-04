e0fb8cfe312da5de63f1fa0a5f33a4cd2ac0b4ee
professorholowczak@gmail.com <professorholowczak@gmail.com>
2025-12-04 16:41:51 +0000

    More bug fixes

--- CIS_4400_Fall_2025_Project/models/agency_dimension.sql
+++ CIS_4400_Fall_2025_Project/models/agency_dimension.sql
@@ -9,5 +9,5 @@ SELECT
     row_number() OVER () AS agency_dim_id,
     agency, agency_name, current_timestamp() as loaded_at
 FROM  agency_columns
-ORDER BY agency, agency_name;
+ORDER BY agency, agency_name
 
