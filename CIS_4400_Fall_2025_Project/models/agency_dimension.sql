f822b3aa62b83eb68412af3cc6aff8703cae935a
professorholowczak@gmail.com <professorholowczak@gmail.com>
2025-12-04 16:31:30 +0000

    Changes to agency_dimension.sql adding ORDER BY clause

--- CIS_4400_Fall_2025_Project/models/agency_dimension.sql
+++ CIS_4400_Fall_2025_Project/models/agency_dimension.sql
@@ -1,8 +1,10 @@
--- agency_dimension.sql
+-- agency_dimension.sql 
 SELECT 
     row_number() OVER () AS agency_dim_id,
     agency, agency_name, current_timestamp() as loaded_at
 FROM  
-   ( SELECT DISTINCT agency, agency_name
-     FROM {{ ref('raw_311_complaints') }}
+    ( 
+         SELECT DISTINCT agency, agency_name
+         FROM {{ ref('raw_311_complaints') }}
+         ORDER BY 1, 2
     )
