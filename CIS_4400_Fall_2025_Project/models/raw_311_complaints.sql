4bab9c020dc892d6b72a0e33f21c9dce85b32707
professorholowczak@gmail.com <professorholowczak@gmail.com>
2025-12-04 13:15:11 +0000

    Materialized incremental on raw_311_complaints.sql

--- CIS_4400_Fall_2025_Project/models/raw_311_complaints.sql
+++ CIS_4400_Fall_2025_Project/models/raw_311_complaints.sql
@@ -1,5 +1,12 @@
+{{ config(materialized='incremental') }}
+
 SELECT *
 FROM  `NYC_311_Complaints.bicycle_complaints_2014`
 UNION DISTINCT
 SELECT *
 FROM  `NYC_311_Complaints.bicycle_complaints_2015`
+
+{% if is_incremental() %}
+  -- this filter will only be applied on incremental runs
+  where updated_at > (select max(updated_at) from {{ this }})
+{% endif %}
