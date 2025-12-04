33731c23dfc93f067d449bd7ba4cc73e623a4cd8
professorholowczak@gmail.com <professorholowczak@gmail.com>
2025-12-04 13:19:05 +0000

    Change updated_at to loaded_at in raw_311_complaints

--- CIS_4400_Fall_2025_Project/models/raw_311_complaints.sql
+++ CIS_4400_Fall_2025_Project/models/raw_311_complaints.sql
@@ -1,12 +1,17 @@
 {{ config(materialized='incremental') }}
 
-SELECT *
-FROM  `NYC_311_Complaints.bicycle_complaints_2014`
-UNION DISTINCT
-SELECT *
-FROM  `NYC_311_Complaints.bicycle_complaints_2015`
+WITH union_311 AS
+(
+   SELECT *
+   FROM  `NYC_311_Complaints.bicycle_complaints_2014`
+   UNION DISTINCT
+   SELECT *
+   FROM  `NYC_311_Complaints.bicycle_complaints_2015`
+)
+SELECT *, current_timestamp() as loaded_at
+FROM union_311  
 
 {% if is_incremental() %}
   -- this filter will only be applied on incremental runs
-  where updated_at > (select max(updated_at) from {{ this }})
+  where loaded_at > (select max(loaded_at) from {{ this }})
 {% endif %}
